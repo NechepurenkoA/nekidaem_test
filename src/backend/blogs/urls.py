64a4ac6915e6e813b374a5898d6736a15fa50e47
NechepurenkoA <nechepurenko.work@gmail.com>
2024-02-16 11:17:32 +0300

    blogs url

--- src/backend/blogs/urls.py
+++ src/backend/blogs/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from blogs.api_views import PostViewSet
+from blogs.api_views import BlogViewSet, PostViewSet
 
 router = DefaultRouter()
 router.register(r"news", PostViewSet, basename="posts")
+router.register(r"blogs", BlogViewSet, basename="follow_blog")
 
 urlpatterns = [
     path(f"{settings.API_V1_PREFIX}/", include(router.urls)),
