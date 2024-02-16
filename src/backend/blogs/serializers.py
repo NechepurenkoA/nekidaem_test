8963fb2fd92aa2768b6b6de3330efe243e9890db
NechepurenkoA <nechepurenko.work@gmail.com>
2024-02-16 11:18:52 +0300

    following and adding post serializers

--- src/backend/blogs/serializers.py
+++ src/backend/blogs/serializers.py
@@ -1,14 +1,30 @@
+from http import HTTPMethod
+
 from rest_framework import serializers
+from rest_framework.request import Request
+from rest_framework.validators import ValidationError
 
-from blogs.models import Blog, Post, PostRead
+from blogs.models import Blog, BlogFollow, Post, PostRead
 
 
-class BlogSerialzier(serializers.ModelSerializer):
+class BlogSerializer(serializers.ModelSerializer):
     """Сериализатор модели 'Blog'."""
+
+    is_followed = serializers.SerializerMethodField(method_name="get_is_followed")
+
+    def get_is_followed(self, blog_id):
+        return BlogFollow.objects.filter(
+            user_id=self.context["request"].user.id,
+            blog_id=blog_id,
+        ).exists()
 
     class Meta:
         model = Blog
-        fields = "__all__"
+        fields = [
+            "id",
+            "is_followed",
+            "followers",
+        ]
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -45,7 +61,42 @@ class PostSerializer(serializers.ModelSerializer):
         return instance
 
 
+class SendToReadSerializer(serializers.Serializer):
+
+    post_id = serializers.IntegerField()
+
+    def validate(self, data):
+        request = self.context["request"]
+        if PostRead.objects.filter(
+            user_id=request.user.id, post_id=data["post_id"]
+        ).exists():
+            raise ValidationError({"error": "Вы уже добавили этот пост в прочитанные!"})
+        return data
+
+
 class FollowBlogSerializer(serializers.Serializer):
     """Сериализатор подписок на блоги других пользователей."""
 
-    ...
+    blog_id = serializers.IntegerField()
+
+    def validate(self, data):
+        request: Request = self.context["request"]
+        if request.user.id == data["blog_id"]:
+            raise ValidationError(
+                {"error": "Нельзя проводить подобные операции с собой!"}
+            )
+        if request.method == HTTPMethod.POST:
+            if BlogFollow.objects.filter(
+                user_id=request.user.id, blog_id=data["blog_id"]
+            ).exists():
+                raise ValidationError(
+                    {"error": "Вы уже подписаны на этого пользователя!"}
+                )
+        if request.method == HTTPMethod.DELETE:
+            if not BlogFollow.objects.filter(
+                user_id=request.user.id, blog_id=data["blog_id"]
+            ).exists():
+                raise ValidationError(
+                    {"error": "Вы не подписаны на этого пользователя!"}
+                )
+        return data
